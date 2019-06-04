d4868ec7d1544239ce571ea17b419ae26eaafdb0
whitesad_01 <whitesad_01@outlook.com>
2019-06-04 23:35:08 +0800

    测试了LM35温度模块

--- Intelligent_dormitory_system_Refrigerator_/Intelligent_dormitory_system_Refrigerator_.ino
+++ Intelligent_dormitory_system_Refrigerator_/Intelligent_dormitory_system_Refrigerator_.ino
@@ -1,26 +1,46 @@
 #include <SoftwareSerial.h>
-#define LED_OUT 2
 
-//使用软件串口，能讲数字口模拟成串口
+/*************Define Variable****************/
+#define LED_OUT 2
+#define LM35_PIN 5
+
+/*************Glonal Variable****************/
+//使用软件串口，能将数字口模拟成串口
 SoftwareSerial BT(8, 9);  //新建对象，接收脚为1，发送脚为0
 
-char val;  //存储接收的变量
-
-void setup() {
-	Serial.begin(9600);   //与电脑的串口连接
+/*************Initialize****************/
+void InitialBluetooth() {
 	Serial.println("BT is ready!");
 	BT.begin(9600);  //设置波特率
 	pinMode(LED_OUT, OUTPUT);
 	digitalWrite(LED_OUT, LOW);
 }
+int InitialLM35(){
+	
+}
+
+/*************Function****************/
+int GetTemperature() {
+	int val;
+	int dat;
+	val = analogRead(LM35_PIN);
+	dat = (125 * val) >> 8;
+	return dat;
+}
+
+
+
+
+void setup() {
+	Serial.begin(9600);   //与电脑的串口连接
+	InitialBluetooth();
+}
 
 void loop() {
-	//如果接收到蓝牙模块的数据，输出到屏幕
-	if (BT.available())
-	{
-		digitalWrite(LED_OUT, HIGH);
-	}
-
+	int tem = GetTemperature();
+	Serial.print(tem);
+	Serial.println("C");
+	delay(10);
 }
 
 //10.175.196.1
